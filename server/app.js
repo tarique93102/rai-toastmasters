9a468f4438a983bec10ed21f612d62ee05b06007
Tarique Ejaz <Tarique.Ejaz@agoda.com>
2019-08-28 16:26:58 +0700

    mongodb connection added

--- server/app.js
+++ server/app.js
@@ -3,15 +3,33 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require('dotenv').config();
+var mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+// takes care of the deprecation messages
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+// database connection establishment and check
+const db_url = process.env.MONGO_DB_URL;
+
+const connect = mongoose.connect(db_url, { useNewUrlParser: true });
+
+connect.then(() => {
+  console.log('Database connection established');
+})
+.catch((err) => console.log(`Error logged: ${err}`))
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+// reduce overhead costs for response data
 app.disable('etag').disable('x-powered-by');
 
 app.use(logger('dev'));
@@ -24,12 +42,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
